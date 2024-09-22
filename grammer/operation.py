ba8cf85c2b35bb110299f17c20c580c178912dfe
gihoon <kiki01111@naver.com>
2024-09-22 14:55:14 +0900

    Feat: folder setting

---
+++ grammer/operation.py
@@ -0,0 +1,9 @@
+# 나누기 나머지 몫
+a=7
+b=3
+print(a/b)
+print(a%b)
+print(a//b)
+
+#거듭제곱 
+print(a**b)
---
+++ sort/sert.py
@@ -0,0 +1,4 @@
+import sys
+input = sys.stdin.readline
+x=int(input())
+a,b=map(int, input().split())
