7ecb446f3925801b648dcb19d8b0571afd195823
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-12-28 01:54:40 +0900

    Solving: greedy 1541

--- greedy/1541.py
+++ greedy/1541.py
@@ -1,12 +1,25 @@
 import sys
 input = sys.stdin.readline
+'''
+55-50+40-10
+10
+'''
 
-express =input().rstrip()
-
-while(express!=""):
-    if express.find('-'):
-        minusIdx=express.index('-')
+'''
+55-(50+40-(10)...)
+'''
+express =input().rstrip() #4-1
+expressArr="" 
+while express!="":
+    minusIdx=express.find('-') #1
+    if minusIdx != -1: #-가 있다면 
+        expressArr=expressArr+express[:minusIdx+1]+'(' # -와 그 앞의 식 + ( 을 배열에 담음
+        express=express[minusIdx+1:] # - 이후의 식으로 식을 업데이트
     else:
-        print(eval(express))
+        if expressArr !="":
+            expressArr+=express +")"
+            print(eval(expressArr))
+        else:
+            print(eval(express))
+        express=""
         break
-print("식에 아무것도 없네요")
